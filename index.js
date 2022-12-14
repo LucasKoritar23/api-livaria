3654b27ff0c8febf88947c6550e157239f8395f9
LucasKoritar23 <>
2022-12-14 15:45:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const strftime = require("strftime");
 const app = express();
-const port = 8000;
+const port = 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
